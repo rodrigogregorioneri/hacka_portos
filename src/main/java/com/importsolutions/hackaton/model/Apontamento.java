a0b1cd962d1093ea25ec41c1cefe07989f6b3bc3
neridev <rodrigogregorioneri@hotmail.com>
2019-04-06 15:58:02 -0300

    Subindo projeto inicial

---
+++ src/main/java/com/importsolutions/hackaton/model/Apontamento.java
@@ -0,0 +1,25 @@
+package com.importsolutions.hackaton.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document(collection = "apontamento")
+@Data
+public class Apontamento {
+	
+	@Id
+	private String id;
+	
+	private String nome_funcionario;
+	
+	private String matricula;
+	
+	private LocalDateTime horario_entrada;
+	
+	private LocalDateTime horario_saida;
+	
+}
